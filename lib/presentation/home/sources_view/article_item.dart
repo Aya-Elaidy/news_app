bf47d62b7dc8de1e9ff4717d495bc2d4b269b474
aya elaidy <ayaelaidy0@gmail.com>
2025-06-28 15:57:28 +0300

    +article details

--- lib/presentation/home/sources_view/article_item.dart
+++ lib/presentation/home/sources_view/article_item.dart
@@ -11,32 +11,101 @@ class ArticleItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: REdgeInsets.all(8),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16.r),
-          border: Border.all(width: 1.w, color: ColorsManager.white)
-      ),
-      child: Column(children: [
-        //Image.network(article.urlToImage ?? ""),
-        CachedNetworkImage(
-          imageUrl:article.urlToImage ?? "",
-          placeholder: (context, url) => CircularProgressIndicator(),
-          errorWidget: (context, url, error) => Icon(Icons.error),
+    return InkWell(
+      onTap: (){
+        showAtricleDetails(article,context);
+      },
+      child: Container(
+        padding: REdgeInsets.all(8),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16.r),
+            border: Border.all(width: 1.w, color: ColorsManager.white)
         ),
-        SizedBox(height: 10.h,),
-        Text(article.title ?? '', style: Theme.of(context).textTheme.titleMedium,),
-        SizedBox(height: 10.h,),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(article.author ?? '', style: Theme.of(context).textTheme.titleSmall,),
+        child: Column(children: [
+          //Image.network(article.urlToImage ?? ""),
+          CachedNetworkImage(
+            imageUrl:article.urlToImage ?? "",
+            placeholder: (context, url) => CircularProgressIndicator(),
+            errorWidget: (context, url, error) => Icon(Icons.error),
+          ),
+          SizedBox(height: 10.h,),
+          Text(article.title ?? '', style: Theme.of(context).textTheme.titleMedium,),
+          SizedBox(height: 10.h,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(article.author ?? '', style: Theme.of(context).textTheme.titleSmall,),
 
-            Text(article.publishedAt ?? '', style: Theme.of(context).textTheme.titleSmall,),
-          ],
-        )
+              Text(article.publishedAt ?? '', style: Theme.of(context).textTheme.titleSmall,),
+            ],
+          )
 
-      ]),
+        ]),
+      ),
     );
   }
+ void showAtricleDetails(Article article,BuildContext context){
+showModalBottomSheet(
+  backgroundColor: Colors.transparent,
+  isScrollControlled: true,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(16.r),
+        topRight: Radius.circular(16.r)
+      )
+    ),
+    context: context,
+    builder:(context){
+  return Container(
+    decoration: BoxDecoration(color: Colors.white,
+    borderRadius: BorderRadius.circular(16.r)),
+    padding: EdgeInsets.all(16),
+    margin: EdgeInsets.all(8),
+    child: Column(
+      //size of only content 
+      mainAxisSize: MainAxisSize.min,
+      //ElevatedButton
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: CachedNetworkImage(
+            imageUrl:article.urlToImage ?? "",
+            placeholder: (context, url) => CircularProgressIndicator(),
+            errorWidget: (context, url, error) => Icon(Icons.error),
+          ),
+        ),
+        Text(article.description??''),
+        ElevatedButton(style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.r)),
+          textStyle:Theme.of(context).textTheme.labelMedium!.copyWith(fontSize: 16.sp)
+
+        ),
+
+
+            onPressed: (){}, child:
+        Text('View Full Article'))
+
+      ],
+    ),
+  );
+});  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
